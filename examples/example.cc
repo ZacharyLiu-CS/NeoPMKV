69c80b5f89db0538cf550a58465a7e6884f45c34
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2022-08-22 15:41:40 +0800

    add test framework

---
+++ examples/example.cc
@@ -0,0 +1,16 @@
+//
+//  example.cc
+//  PROJECT example
+//
+//  Created by zhenliu on 22/08/2022.
+//  Copyright (c) 2022 zhenliu <liuzhenm@mail.ustc.edu.cn>.
+//
+
+#include <iostream>
+#include "add.h"
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Hello world"  << add(1,2) << std::endl;
+  return 0;
+}
---
+++ include/add.h
@@ -0,0 +1,15 @@
+//
+//  add.h
+//  PROJECT add
+//
+//  Created by zhenliu on 22/08/2022.
+//  Copyright (c) 2022 zhenliu <liuzhenm@mail.ustc.edu.cn>.
+//
+
+#pragma once
+
+#include <cstdint>
+
+int32_t add(int32_t a, int32_t b){
+  return a+b;
+}

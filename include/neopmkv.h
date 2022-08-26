dbcfed8ae2f391fd3551b818b6d5104477715e0c
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2022-08-26 10:16:47 +0800

    add shard to reduce mutex overhead

--- include/neopmkv.h
+++ include/neopmkv.h
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include <string>
 #include "buffer_page.h"
 #include "logging.h"
 #include "pbrb.h"
@@ -21,29 +22,32 @@ namespace NKV {
 
 using EntryKey = uint64_t;
 using EntryValue = PmemAddress;
+#define SHARD_NUM 64
+
 class NeoPMKV {
  public:
   NeoPMKV(std::string db_path = "/mnt/pmem0/tmp-neopmkv",
           uint64_t chunk_size = 16ULL << 20, uint64_t db_size = 16ULL << 30) {
-    _engine_config.chunk_size = chunk_size;
-    _engine_config.engine_capacity = db_size;
-    strcpy(_engine_config.engine_path, db_path.c_str());
-    NKV::PmemEngine::open(_engine_config, &_engine_ptr);
+    for (uint32_t i = 0; i < SHARD_NUM; i++) {
+      _engine_config[i].chunk_size = chunk_size;
+      _engine_config[i].engine_capacity = db_size;
+      std::string shard_path = db_path + "/shard" + std::to_string(i);
+      strcpy(_engine_config[i].engine_path, shard_path.c_str());
+      NKV::PmemEngine::open(_engine_config[i], &_engine_ptr[i]);
+    }
   }
-  ~NeoPMKV() {
-    delete _engine_ptr;
-  }
+  ~NeoPMKV() {for(uint32_t i = 0; i< SHARD_NUM ;i++) delete _engine_ptr[i]; }
   bool get(EntryKey key, std::string &value);
   bool put(EntryKey &key, const std::string &value);
   bool remove(EntryKey &key);
-  bool scan(EntryKey &start, std::vector<std::string>& value_list,
+  bool scan(EntryKey &start, std::vector<std::string> &value_list,
             uint32_t scan_len);
 
  private:
-  std::map<EntryKey, EntryValue> _indexer;
-
-  PmemEngineConfig _engine_config;
-  PmemEngine *_engine_ptr = nullptr;
+  std::map<EntryKey, EntryValue> _indexer[SHARD_NUM];
+  std::mutex _mutex[SHARD_NUM];
+  PmemEngineConfig _engine_config[SHARD_NUM];
+  PmemEngine *_engine_ptr[SHARD_NUM];
 };
 
 }  // namespace NKV
--- src/neopmkv.cc
+++ src/neopmkv.cc
@@ -8,17 +8,20 @@
 
 #include "neopmkv.h"
 #include <cstdint>
+#include <mutex>
 
 namespace NKV {
 bool NeoPMKV::get(EntryKey key, std::string &value) {
-  if (_indexer.find(key) != _indexer.end()) {
-    EntryValue value_ptr = _indexer[key];
+  uint32_t idx = key % SHARD_NUM;
+  std::lock_guard<std::mutex> guard(_mutex[idx]);
+  if (_indexer[idx].find(key) != _indexer[idx].end()) {
+    EntryValue value_ptr = _indexer[idx][key];
     if (value_ptr == UINT64_MAX) {
       {
         return false;
       }
     }
-    auto s = _engine_ptr->read(value_ptr, value);
+    auto s = _engine_ptr[idx]->read(value_ptr, value);
     if (s.is2xxOK())
       return true;
     else
@@ -27,29 +30,35 @@ bool NeoPMKV::get(EntryKey key, std::string &value) {
   return false;
 }
 bool NeoPMKV::put(EntryKey &key, const std::string &value) {
+  uint32_t idx = key % SHARD_NUM;
+  std::lock_guard<std::mutex> guard(_mutex[idx]);
   EntryValue value_ptr;
   Status s =
-      _engine_ptr->append(value_ptr, (char *)value.c_str(), value.size());
+      _engine_ptr[idx]->append(value_ptr, (char *)value.c_str(), value.size());
   if (s.is2xxOK()) {
-    _indexer[key] = value_ptr;
+    _indexer[idx][key] = value_ptr;
     return true;
   }
   return false;
 }
 bool NeoPMKV::remove(EntryKey &key) {
-  if (_indexer.find(key) != _indexer.end()) {
-    _indexer[key] = UINT64_MAX;
+  uint32_t idx = key % SHARD_NUM;
+  std::lock_guard<std::mutex> guard(_mutex[idx]);
+  if (_indexer[idx].find(key) != _indexer[idx].end()) {
+    _indexer[idx][key] = UINT64_MAX;
     return true;
   }
   return false;
 }
 bool NeoPMKV::scan(EntryKey &start, std::vector<std::string> &value_list,
                    uint32_t scan_len) {
+  uint32_t idx = start % SHARD_NUM;
+  std::lock_guard<std::mutex> guard(_mutex[idx]);
   value_list.reserve(scan_len);
-  auto iter = _indexer.upper_bound(start);
-  for (auto i = 0; i < scan_len && iter != _indexer.end(); i++) {
+  auto iter = _indexer[idx].upper_bound(start);
+  for (auto i = 0; i < scan_len && iter != _indexer[idx].end(); i++) {
     std::string tmp_value;
-    _engine_ptr->read(iter->second, tmp_value);
+    _engine_ptr[idx]->read(iter->second, tmp_value);
     value_list.push_back(tmp_value);
     iter++;
   }

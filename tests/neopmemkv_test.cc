b4ee755431c8e7f1e4f19937dd19fe1e306d373f
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2022-09-08 17:48:51 +0800

    remove neopmkv_test

--- tests/neopmemkv_test.cc
+++ tests/neopmemkv_test.cc
@@ -27,7 +27,15 @@ std::vector<SchemaField> Fields{SchemaField(FieldType::INT64T, "pk"),
                                 SchemaField(FieldType::STRING, "f2", 16)};
 const uint64_t chunk_size = 128ull << 20;
 const uint64_t db_size = 1ull << 30;
-
+void cleanFile(std::string file_path) {
+  bool status = false;
+  if (std::filesystem::exists(file_path)) {
+    status = std::filesystem::remove_all(file_path);
+  } else {
+    status = true;
+  }
+  ASSERT_TRUE(status == true);
+}
 void PBRBTest(bool enablePBRB) {
   int res = system(clean_cmd.c_str());
   res = system(mkdir_cmd.c_str());
@@ -46,10 +54,12 @@ void PBRBTest(bool enablePBRB) {
     std::string num_str = std::to_string(i);
     int zero_padding = 32 - num_str.size();
     Value v;
-    v.append(zero_padding,'0').append(num_str);
+    v.append(zero_padding, '0').append(num_str);
     return v;
   };
-  auto BuildKey = [sid](uint64_t i) -> Key { return Key(sid, std::to_string(i)); };
+  auto BuildKey = [sid](uint64_t i) -> Key {
+    return Key(sid, std::to_string(i));
+  };
 
   for (uint64_t i = 0; i < length; i++) {
     auto key = BuildKey(i);
@@ -82,24 +92,24 @@ void PBRBTest(bool enablePBRB) {
   std::vector<Value> value_list;
   int scan_length = 100;
   neopmkv_->scan(start_key, value_list, scan_length);
-  for(uint64_t i = 0;i < value_list.size();i++){
-    auto expect_value = BuildValue(i+remove_length);
-    NKV_LOG_D(std::cout,"Key [{}] expect value: {} read value: {}",i+remove_length, expect_value, value_list[i]);
+  for (uint64_t i = 0; i < value_list.size(); i++) {
+    auto expect_value = BuildValue(i + remove_length);
+    NKV_LOG_D(std::cout, "Key [{}] expect value: {} read value: {}",
+              i + remove_length, expect_value, value_list[i]);
     ASSERT_EQ(expect_value, value_list[i]);
   }
   delete neopmkv_;
 }
-TEST(NEOPMKVTEST, DisablePBRBTest) { PBRBTest(false); }
-TEST(NEOPMKVTEST, CleanTestFile){
-  bool status = false;
-  if (std::filesystem::exists(db_path)) {
-    status = std::filesystem::remove_all(db_path);
-  } else {
-    status = true;
-  }
-  ASSERT_TRUE(status == true);
+TEST(NEOPMKVTEST, DisablePBRBTest) {
+  PBRBTest(false);
+  cleanFile(db_path);
 }
-TEST(NEOPMKVTEST, EnablePBRBTest) { PBRBTest(true); }
+
+TEST(NEOPMKVTEST, EnablePBRBTest) {
+  PBRBTest(true);
+  cleanFile(db_path);
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
